21eec806becd24e755b6dd97c21faee25c453c1d
Nicholas Gulachek <nsgulachek@gmail.com>
2024-12-23 15:14:03 -0600

    Implement custom require logic to avoid caching

--- src/spec/WebpackRuleSpec.ts
+++ src/spec/WebpackRuleSpec.ts
@@ -1,24 +1,44 @@
 import { expect } from "chai";
-import { mkdir, rm, writeFile, stat } from "node:fs/promises";
+import { mkdir, rm, writeFile, stat, readFile } from "node:fs/promises";
 import { Makefile, Path, updateTarget } from "esmakefile";
 import { addWebpack } from "../index.js";
 import { chdir, cwd } from "node:process";
-import { createRequire } from "node:module";
+import { join, resolve } from "node:path";
 
-const require = createRequire(new URL(import.meta.url));
+function loadModule(this: any, content: string, exportName: string): any {
+  eval(content);
+  return this[exportName];
+}
+
+async function myRequire(path: string): Promise<any> {
+  const jsContent = await readFile(path, "utf8");
+  return loadModule.call({}, jsContent, "TestModule");
+}
 
 describe("addWebpack", function () {
   this.timeout(5000); // 5s becuz webpack slow
 
-  const dir = ".scratch/webpack-rule-spec";
+  const dir = resolve(".scratch/webpack-rule-spec");
   let _cwd: string;
   let make: Makefile;
 
   const entry = "./entry.js";
-  const output = Path.build("entry.cjs");
+  const output = Path.build("entry.js");
 
-  function resetRequire() {
-    delete require.cache[make.abs(output)];
+  function addWp() {
+    addWebpack(make, {
+      entry,
+      target: ["node"],
+      output: {
+        path: make.buildRoot,
+        filename: output.basename,
+        library: {
+          type: "this",
+          name: "TestModule",
+        },
+      },
+      mode: "production",
+    });
   }
 
   beforeEach(async () => {
@@ -27,11 +47,12 @@ describe("addWebpack", function () {
     _cwd = cwd();
     chdir(dir);
 
+    await writeFile(join(dir, "package.json"), '{"type": "commonjs"}');
+
     make = new Makefile();
   });
 
   afterEach(async () => {
-    resetRequire();
     chdir(_cwd);
     await rm(dir, { recursive: true, force: true });
   });
@@ -39,63 +60,38 @@ describe("addWebpack", function () {
   it('registers a "webpack" target', async () => {
     await writeFile(entry, "module.exports = { four: 4 };");
 
-    addWebpack(make, {
-      entry,
-      target: "node",
-      output: {
-        path: make.buildRoot,
-        filename: output.basename,
-        library: {
-          type: "commonjs",
-        },
-      },
-      mode: "production",
-    });
+    addWp();
 
     const result = await updateTarget(make, "webpack");
     expect(result).to.be.true;
 
-    const mod = require(make.abs(output));
+    const mod = await myRequire(make.abs(output));
     expect(mod.four).to.equal(4);
   });
 
   it("rebuilds if entrypoint changes", async () => {
     await writeFile(entry, "module.exports = { four: 4 };");
 
-    addWebpack(make, {
-      entry,
-      target: "node",
-      output: {
-        path: make.buildRoot,
-        filename: output.basename,
-        library: {
-          type: "commonjs",
-        },
-      },
-      mode: "production",
-    });
+    addWp();
 
     let result = await updateTarget(make, "webpack");
     expect(result).to.be.true;
 
-    const cjsMod = make.abs(output);
+    const modScript = make.abs(output);
 
-    const mod = require(cjsMod);
+    const mod = await myRequire(modScript);
     expect(mod.four).to.equal(4);
 
     await writeFile(entry, "module.exports = { five: 5 };");
     result = await updateTarget(make, "webpack");
     expect(result).to.be.true;
 
-    resetRequire();
-
-    const newMod = require(cjsMod);
+    const newMod = await myRequire(modScript);
     expect(newMod.five).to.equal(5);
   });
 
   it("rebuilds when a transitive dependency changes", async () => {
     const dep = "./dep.js";
-    await writeFile("./package.json", '{"type": "commonjs"}');
     await writeFile(dep, "module.exports = { n: 4 };");
     await writeFile(
       entry,
@@ -105,56 +101,32 @@ describe("addWebpack", function () {
 										`
     );
 
-    addWebpack(make, {
-      entry,
-      target: "node",
-      output: {
-        path: make.buildRoot,
-        filename: output.basename,
-        library: {
-          type: "commonjs",
-        },
-      },
-      mode: "production",
-    });
+    addWp();
 
     let result = await updateTarget(make, "webpack");
     expect(result).to.be.true;
 
-    const cjsMod = make.abs(output);
+    const testMod = make.abs(output);
 
-    const mod = require(cjsMod);
+    const mod = await myRequire(testMod);
     expect(mod.n).to.equal(4);
 
     await writeFile(dep, "module.exports = { n: 5 };");
     result = await updateTarget(make, "webpack");
     expect(result).to.be.true;
 
-    resetRequire();
-
-    const newMod = require(cjsMod);
+    const newMod = await myRequire(testMod);
     expect(newMod.n).to.equal(5);
   });
 
   it("does not rebuild if entry does not change", async () => {
     await writeFile(entry, "module.exports = { four: 4 };");
 
-    addWebpack(make, {
-      entry,
-      target: "node",
-      output: {
-        path: make.buildRoot,
-        filename: output.basename,
-        library: {
-          type: "commonjs",
-        },
-      },
-      mode: "production",
-    });
+    addWp();
 
     let result = await updateTarget(make, "webpack");
     expect(result).to.be.true;
-    const mod = require(make.abs(output));
+    const mod = await myRequire(make.abs(output));
     expect(mod.four).to.equal(4);
 
     const oldStats = await stat(make.abs(Path.build("webpack")));
